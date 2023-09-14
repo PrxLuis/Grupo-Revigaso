6f90500007e59951c2887578dc63c8052a65cebc
PrxLuis <prxluis@outlook.es>
2023-09-14 00:19:11 -0600

    últimas modificaciones responsive, enlace de WhatsApp listo y formulario con token

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,12 +45,26 @@ const swiper2 = new Swiper('.swiper-projects', {
 });
 
 //SCROLL REVEAL
-const sr = ScrollReveal({
+// ScrollReveal().reveal('.anexo-description, .conoceBtn, .agendarBtn-container', { delay: 500, distance: '50px', duration: 600, easing: 'ease-in', opacity: 0, origin: 'bottom', rotate: { x:20, z:20}, scale: 5, cleanup: false, useDelay: 'once' });
+ScrollReveal().reveal('.anexo-description, .action, .swiper-pagination', { distance: '50px', origin: 'bottom', duration: 2000})
+ScrollReveal().reveal('.flama-revigaso, .logo-container', { scale: 1, duration: 2500})
+ScrollReveal().reveal('.left-t-sr', {distance: '100px', origin: 'top', duration: 1000, easing: 'ease-in'})
+ScrollReveal().reveal('.right-t-sr, .titulo-rmf', {distance: '100px', origin: 'left', duration: 1000, easing: 'ease-in'})
+
+ScrollReveal().reveal('.m-v-box', {duration: 200, interval: 500, easing: 'ease-in'})
+ScrollReveal().reveal('.service-anexo', {duration: 200, interval: 500, easing: 'ease-in'})
+ScrollReveal().reveal('.swiper-wrapper', {duration: 200, interval: 500, easing: 'ease-in'})
+ScrollReveal().reveal('.sr-h', {duration: 200, interval: 500, easing: 'ease-in'})
+ScrollReveal().reveal('.reason-container', {duration: 200, interval: 500, easing: 'ease-in'})
+ScrollReveal().reveal('.box-container', {duration: 200, interval: 500, easing: 'ease-in'})
+
+// interval: 600
+/* const sr = ScrollReveal({
   origin: 'top',
   distance: '60px',
   duration: 2500,
   delay: 400
-});
+}); */
 /* sr.reveal(`h1`,{origin: 'left', reveal: 500})
 sr.reveal(`.llamaContainer`,{origin:'right'})
 sr.reveal(`.conoceBtn, .agendarBtn-container`)
